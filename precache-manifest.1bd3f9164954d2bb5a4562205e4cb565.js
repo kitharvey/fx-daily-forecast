97aa42c23545ff6478132fcbcb0dc8edda9a7a52
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-06-18 21:40:16 +0800

    Updates

---
+++ precache-manifest.1bd3f9164954d2bb5a4562205e4cb565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1953262a715ff7a1d7e6c1dd3c006c83",
+    "url": "/fx-daily-forecast/index.html"
+  },
+  {
+    "revision": "7f5f4a2ec16b15b86704",
+    "url": "/fx-daily-forecast/static/css/main.af3b8750.chunk.css"
+  },
+  {
+    "revision": "ab5330cad838e3f32bd6",
+    "url": "/fx-daily-forecast/static/js/2.51ea4069.chunk.js"
+  },
+  {
+    "revision": "583ce86d7f120f4227d5f98b4c47211e",
+    "url": "/fx-daily-forecast/static/js/2.51ea4069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5f4a2ec16b15b86704",
+    "url": "/fx-daily-forecast/static/js/main.b4ca45b9.chunk.js"
+  },
+  {
+    "revision": "cbe43cf9c37e049fb01a",
+    "url": "/fx-daily-forecast/static/js/runtime-main.1f9fff69.js"
+  }
+]);
