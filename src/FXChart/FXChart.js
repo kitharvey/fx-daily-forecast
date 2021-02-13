135f67d37a94301d137de7f74dddbf6a49749ae3
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-02-13 19:51:30 +0800

    signals was yeeted

--- src/FXChart/FXChart.js
+++ src/FXChart/FXChart.js
@@ -1,24 +1,55 @@
 import axios from 'axios';
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
 import { useQuery } from 'react-query';
-// import Chart from 'react-apexcharts'
-import { Area,
-    AreaChart, 
-    CartesianGrid,
-    ResponsiveContainer, 
-    Tooltip, 
-    // XAxis, 
-    YAxis } from 'recharts';
+
 import '../sass/FXChart.scss'
 
 const FXChart = ({pair}) => {
     const { data, isFetching } = useQuery(['fetchChart', pair], async() => {
-        const {data} = await axios.get(`https://fcsapi.com/api-v3/forex/history?symbol=${pair}&period=1d&access_key=b05Bp18k1jkg0pVrjv11EhvQk0aseUUIO6ecMM1sJecSbP8M8G`)
-        return await data.response ? Object.values(data.response) : null
+        const {data} = await axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${pair}?apikey=93cda20ae659089e470d6b3c63af17f3`)
+        return await data ? data.historical.sort().reverse() : null
     }  )
 
-    const getPrice = ({o,c,h,l, tm}) => {
-        return {date: tm, price: ((parseFloat(o) + parseFloat(c) + parseFloat(h) + parseFloat(l))/4).toFixed(4)}
+    const options = {
+        chart: {
+          type: 'area',
+        },
+        xaxis: {
+          type: 'datetime'
+        },
+        yaxis: {
+          tooltip: {
+            enabled: true
+          }
+        },
+        dataLabels: {
+            enabled: false
+          },
+        stroke: {
+            show: true,
+            curve: 'smooth',
+            colors: undefined,
+            width: 1,
+            dashArray: 0,      
+        },
+        tooltip: {
+            x: {
+                show: true,
+                format: 'dd MMM yyyy',
+                formatter: undefined,
+            },
+        }
+        
+      }
+
+    const getPrice = (data) => {
+        return [{name: "Price", data: data.map( ({open,close,high,low, date}) => {
+            return {
+                x: date,
+                y: ((open + high + low + close)/4).toFixed(4)
+            }
+        }  )}] 
     }
 
     const CustomTooltip = ({ active, payload }) => {
@@ -41,50 +72,7 @@ const FXChart = ({pair}) => {
             : !data ? <div className='error'>Access blocked. Restriction remove after 1 minute.</div>
                 :   <div className = 'chart'>
                         <h3> {`${pair} Daily Chart`}  </h3>
-                        <ResponsiveContainer height='90%' >
-                            <AreaChart
-                                data={data.map( ( price ) => getPrice(price) )}
-                                margin={{top: 0, right: 5, left: 5}}
-                            >
-                            <defs>
-                                <linearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor={getPrice(data[data.length-1]).price > getPrice(data[data.length-2]).price ? "#00BAEC" : "#FF6347"} stopOpacity={1} />
-                                <stop offset="95%" stopColor={getPrice(data[data.length-1]).price > getPrice(data[data.length-2]).price ? "#00BAEC00" : "#FF634700"}stopOpacity={1} />
-                                </linearGradient>
-                            </defs>
-                            <CartesianGrid
-                                strokeDasharray="0 "
-                                vertical={false}
-                                strokeOpacity={0.5}
-                            />
-                            <Tooltip payload={data} content={CustomTooltip} />
-                            <YAxis
-                                hide={false}
-                                domain={['dataMin', 'auto']}
-                                tickCount={10}
-                                interval={0}
-                                strokeOpacity={0}
-                                fillOpacity={1}
-                                width={40}
-                                style={{
-                                fontStyle: "normal",
-                                fontWeight: "600",
-                                fontSize: "12px",
-                                lineHeight: "9px",
-                                }}
-                            />
-                            <Area
-                                // isAnimationActive={false}
-                                type="monotone"
-                                dataKey="price"
-                                stroke={getPrice(data[data.length-1]).price > getPrice(data[data.length-2]).price ? "#00BAEC" : "#FF6347"}
-                                strokeWidth={2}
-                                fillOpacity={1}
-                                fill="url(#grad)"
-                            >
-                            </Area>
-                            </AreaChart>
-                        </ResponsiveContainer>
+                         {data && <ReactApexChart options={options} series={getPrice(data)} type="area" height='95%' width='100%' />} 
                     </div>}
         </div>
     )
--- src/FXChart/FXLatestPrice.js
+++ src/FXChart/FXLatestPrice.js
@@ -6,17 +6,13 @@ import FXChart from './FXChart'
 // import FXSignals from './FXSignals'
 
 const FXLatestPrice = () => {
-    const [pair, setPair] = useState('EUR/USD')
-
+    const [pair, setPair] = useState('EURUSD')
+    useState(2000)
     const { data } = useQuery('fetchLatestPrice', async() => {
-        const {data} = await axios.get(`https://fcsapi.com/api-v3/forex/latest?symbol=all_forex&access_key=b05Bp18k1jkg0pVrjv11EhvQk0aseUUIO6ecMM1sJecSbP8M8G`)
-        console.log(data.response)
-        return await data.response
-    }  )
+        const {data} = await axios.get(`https://financialmodelingprep.com/api/v3/quotes/forex?apikey=93cda20ae659089e470d6b3c63af17f3`)
+        return await data
+    })
 
-    // const handleOnClick = (event) => {
-    //     setPair(event.currentTarget.textContent.split(' ')[1])
-    // }
 
     return (
         <div className='container'>
@@ -28,13 +24,13 @@ const FXLatestPrice = () => {
                     <h3> Change </h3>
                 </div>
                 <div className = 'latestPriceContainer'>
-                    {data.map(({id,s,c,ch,cp}) => (
-                        <div className = {`latestPrice ${s === pair ? 'active' : ''}`} key={id} onClick = {() => setPair(s)}>
-                            <div className = 'symbol'>{s}</div>
-                            <div className = 'price'>{parseFloat(c).toFixed(4)}</div>
+                    {data.map(({name,symbol,price,change,changesPercentage}) => (
+                        <div className = {`latestPrice ${name === pair ? 'active' : ''}`} key={symbol} onClick = {() => setPair(symbol)}>
+                            <div className = 'symbol'>{name}</div>
+                            <div className = 'price'>{price.toFixed(4)}</div>
                             <div className = 'change'>
-                                <div className = {parseFloat(ch).toFixed(4)>0 ? 'positive' : 'negative'}>{parseFloat(ch).toFixed(4)}</div>
-                                <div className = {parseFloat(cp).toFixed(2)>0 ? 'positive' : 'negative'}>{parseFloat(cp).toFixed(2)}%</div>
+                                <div className = {change.toFixed(4)>0 ? 'positive' : 'negative'}>{change.toFixed(4)}</div>
+                                <div className = {changesPercentage.toFixed(2)>0 ? 'positive' : 'negative'}>{changesPercentage.toFixed(2)}%</div>
                             </div>
                         </div>
                     ))}
